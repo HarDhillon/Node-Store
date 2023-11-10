256e93bff5a9c9abb84b844418ffbbe0ac75a034
HarDhillon <har7173@gmail.com>
2023-11-10 11:59:43 +0000

    refactor and remove vanilla node.js with express functions

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-
-const http = require('http');
 const express = require('express')
 
 const app = express()
@@ -13,7 +11,4 @@ app.use((req, res, next) => {
     console.log('In another middleware!')
     res.send('<h1>hello from express</h1>');
 })
-
-const server = http.createServer(app)
-
-server.listen(3000)
+app.listen(3000)
