5c0f3b2f8f78b3f129d445d6817efa566d21f98d
HarDhillon <har7173@gmail.com>
2023-11-10 16:53:27 +0000

    updating routing for /admin

--- app.js
+++ app.js
@@ -8,7 +8,12 @@ const shopRoutes = require('./routes/shop')
 // bodyParser is now part of express so we can actually just do this
 app.use(express.urlencoded({ extended: true }))
 
-app.use(adminRoutes)
+// putting a /argument here will only run if the url is /admin/*something*
+app.use('/admin', adminRoutes)
 app.use(shopRoutes)
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>')
+})
 
 app.listen(3000)
