950b3e8eb66c2da71c5f1c1e6fb8cc8fd5104ce4
HarDhillon <har7173@gmail.com>
2023-11-10 13:16:54 +0000

    parsing incoming data from user posting

--- app.js
+++ app.js
@@ -2,13 +2,20 @@ const express = require('express')
 
 const app = express()
 
-app.use((req, res, next) => {
-    console.log('In the middleware!')
+// bodyParser is now part of express so we can actually just do this
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/add-product', (req, res, next) => {
     // next sends us to the next middleware
-    next();
+    res.send('<form action="/product" method="POST"> <input type="text" name="title" /> <button type="submit">Add Product</button> </form>');
 })
+
+app.post('/product', (req, res, next) => {
+    console.log(req.body)
+    res.redirect('/')
+})
+
 app.use((req, res, next) => {
-    console.log('In another middleware!')
     res.send('<h1>hello from express</h1>');
 })
 app.listen(3000)
