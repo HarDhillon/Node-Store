4cea027bf4a46fa85409d73ce51ce85e02601ed6
HarDhillon <har7173@gmail.com>
2023-11-16 11:51:12 +0000

    adding a helperfunction to find our root directory

--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,12 @@
 const path = require('path')
+const rootDir = require('../util/path')
 
 const express = require('express')
 const router = express.Router()
 
 // get / post etc uses EXACT match to path
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '..', 'views', 'shop.html'))
+    res.sendFile(path.join(rootDir, 'views', 'shop.html'))
 })
 
 module.exports = router
---
+++ util/path.js
@@ -0,0 +1,4 @@
+const path = require('path')
+
+// Get the path to our root directory
+module.exports = path.dirname(require.main.filename);
