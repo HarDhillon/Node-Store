c01c7b3c90039ed22740ea188dcc8ca1e9965079
HarDhillon <har7173@gmail.com>
2023-11-17 13:54:01 +0000

    adding styling and serving a static css file from public directory

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 const express = require('express')
 
+const path = require('path')
+const rootDir = require('./util/path')
+
 const app = express()
 
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
-const notFoundRoutes = require('./routes/404')
 
 // bodyParser is now part of express so we can actually just do this
 app.use(express.urlencoded({ extended: true }))
+
+app.use(express.static(path.join(rootDir, 'public')))
 
 // putting a /argument here will only run if the url is /admin/*something*
 app.use('/admin', adminRoutes)
 app.use(shopRoutes)
 
-app.use(notFoundRoutes)
+// Any other routes not matching
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'))
+})
 
 app.listen(3000)
