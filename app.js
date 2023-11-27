c6bcdfea5570ec1ac69bf3c0c07cc5bde8876683
HarDhillon <har7173@gmail.com>
2023-11-27 15:30:13 +0000

    using EJS templating

--- app.js
+++ app.js
@@ -2,13 +2,10 @@ const express = require('express')
 
 const path = require('path')
 const rootDir = require('./util/path')
-const expressHbs = require('express-handlebars')
 
 const app = express()
 
-app.engine('hbs', expressHbs({ layoutsDir: 'views/layouts', defaultLayout: 'main-layout', extname: 'hbs' }))
-app.set('view engine', 'hbs');
-app.set()
+app.set('view engine', 'ejs');
 
 const adminData = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
