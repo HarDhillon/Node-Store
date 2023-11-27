8846b678ed9a16846e7d1c6832e5ec7ae4042dfb
HarDhillon <har7173@gmail.com>
2023-11-27 11:13:03 +0000

    adds pug ejs and handlebars

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const rootDir = require('./util/path')
 
 const app = express()
 
-const adminRoutes = require('./routes/admin')
+const adminData = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
 // bodyParser is now part of express so we can actually just do this
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(rootDir, 'public')))
 
 // putting a /argument here will only run if the url is /admin/*something*
-app.use('/admin', adminRoutes)
+app.use('/admin', adminData.routes)
 app.use(shopRoutes)
 
 // Any other routes not matching
--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,13 @@
 const path = require('path')
 const rootDir = require('../util/path')
+const adminData = require('./admin')
 
 const express = require('express')
 const router = express.Router()
 
 // get / post etc uses EXACT match to path
 router.get('/', (req, res, next) => {
+    console.log(adminData.products)
     res.sendFile(path.join(rootDir, 'views', 'shop.html'))
 })
 
