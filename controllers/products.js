94ff206e7533926d00d29acead70007bf4146e2c
HarDhillon <har7173@gmail.com>
2023-12-04 15:21:33 +0000

    passing fetchAll an anonymous function to ensure the file is read before we send the response

--- controllers/products.js
+++ controllers/products.js
@@ -17,13 +17,16 @@ exports.postAddProduct = (req, res, next) => {
 }
 
 exports.getProducts = (req, res, next) => {
-    const products = Product.fetchAll();
-    res.render('shop', {
-        prods: products,
-        pageTitle: 'Shop',
-        path: "/",
-        hasProducts: products.length > 0,
-        activeShop: true,
-        productCSS: true,
+    // We call fetchAll and pass it an anonymous function
+    Product.fetchAll(products => {
+        res.render('shop', {
+            prods: products,
+            pageTitle: 'Shop',
+            path: "/",
+            hasProducts: products.length > 0,
+            activeShop: true,
+            productCSS: true,
+        })
     })
+
 }
--- models/product.js
+++ models/product.js
@@ -1,4 +1,5 @@
-const products = []
+const fs = require('fs')
+const path = require('path')
 
 module.exports = class Product {
     constructor(t) {
@@ -6,10 +7,35 @@ module.exports = class Product {
     }
 
     save() {
-        products.push(this)
+        // path to our file with the products
+        const p = path.join(path.dirname(require.main.filename), 'data', 'products.json');
+
+        fs.readFile(p, (err, fileConent) => {
+            let products = []
+            // Grab all the products from the file if no error
+            if (!err) {
+                products = JSON.parse(fileConent)
+            }
+            // add the created instance of the class to the end of the other products (if they exist)
+            products.push(this)
+            // Write the latest array of products to file
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err)
+            })
+        })
     }
 
-    static fetchAll() {
-        return products
+    // By passing a callback - we ensure that the function doesnt run until the file is read
+    static fetchAll(cb) {
+
+        const p = path.join(path.dirname(require.main.filename), 'data', 'products.json');
+
+
+        fs.readFile(p, (err, fileConent) => {
+            if (err) {
+                cb([])
+            }
+            cb(JSON.parse(fileConent))
+        })
     }
 }
