1c4a247db29e61101a8c38dd1d602f7e9f82e791
HarDhillon <har7173@gmail.com>
2024-01-16 15:08:35 +0000

    Add functionality to find then show product detail

--- models/product.js
+++ models/product.js
@@ -35,8 +35,17 @@ module.exports = class Product {
         })
     }
 
+    // static methods get called on an a class object not an object. So you call it as Product.method
+
     // By passing a callback - we ensure that the function doesnt run until the file is read
     static fetchAll(cb) {
         getProductsFromFile(cb)
+    }
+
+    static findById(id, cb) {
+        getProductsFromFile(products => {
+            const product = products.find(p => p.id === id)
+            cb(product)
+        })
     }
 }
