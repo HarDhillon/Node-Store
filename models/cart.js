c87bdc30206221860f6e68e7fa23e95ad0a5227e
HarDhillon <har7173@gmail.com>
2024-01-26 16:40:13 +0000

    Create new Cart model and track user cart in cart.json. Update controller postCart to work with new model

---
+++ models/cart.js
@@ -0,0 +1,44 @@
+const fs = require('fs')
+const path = require('path')
+
+// Path to cart data
+const p = path.join(path.dirname(require.main.filename), 'data', 'cart.json');
+
+module.exports = class Cart {
+
+    static addProduct(id, productPrice) {
+        // Create cart object and add cart objects to it if already exist
+        fs.readFile(p, (err, fileContent) => {
+            let cart = { products: [], totalPrice: 0 }
+            if (!err) {
+                cart = JSON.parse(fileContent)
+            }
+            // Check if product has been added to cart before
+            const existingProductIndex = cart.products.findIndex(prod => prod.id === id)
+            const existingProduct = cart.products[existingProductIndex]
+            let updatedProduct;
+            // If product is already in cart
+            if (existingProduct) {
+                // Create new object to replace old object
+                updatedProduct = { ...existingProduct }
+                // update product quantity
+                updatedProduct.qty = updatedProduct.qty + 1
+                // Replace old product with new updated product
+                cart.products = [...cart.products]
+                cart.products[existingProductIndex] = updatedProduct
+            }
+            // Otherwise create new object in cart
+            else {
+                updatedProduct = { id: id, qty: 1 };
+                cart.products = [...cart.products, updatedProduct]
+            }
+            // Update total price
+            cart.totalPrice = cart.totalPrice + +productPrice
+            // Update cart data
+            fs.writeFile(p, JSON.stringify(cart), err => {
+                console.log(err)
+            })
+        })
+    }
+
+}
