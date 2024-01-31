a794260f901e79e39a2db04b9b469e536effd89b
HarDhillon <har7173@gmail.com>
2024-01-31 17:02:07 +0000

    pass editing variable with getAddProduct so variable exists in view and can render

--- controllers/admin.js
+++ controllers/admin.js
@@ -4,6 +4,7 @@ exports.getAddProduct = (req, res, next) => {
     res.render('admin/edit-product', {
         pageTitle: 'Add product',
         path: '/admin/add-product',
+        editing: null,
     })
 }
 
