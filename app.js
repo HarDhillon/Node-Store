b5d3417bd5053ed0f24c62514c2541d2cdd0e002
HarDhillon <har7173@gmail.com>
2024-02-08 17:42:38 +0000

    create new user Model, update app.js to create relations between users and products. Fetch a dummy user in app.js in our reqeust

--- app.js
+++ app.js
@@ -8,12 +8,24 @@ const shopRoutes = require('./routes/shop');
 const errorController = require('./controllers/error');
 
 const sequelize = require('./util/database')
+const Product = require('./models/product')
+const User = require('./models/user')
 
 app.set('view engine', 'ejs');
 // bodyParser is now part of express so we can actually just do this
 app.use(express.urlencoded({ extended: true }))
 // Determine where static files will come from
 app.use(express.static(path.join(rootDir, 'public')))
+
+// Store our sequelize object in our request.
+app.use((req, res, next) => {
+    User.findByPk(1)
+        .then(user => {
+            req.user = user
+            next()
+        })
+        .catch(err => console.log(err))
+})
 
 // putting a /argument here will only run if the url is /admin/*something*
 app.use('/admin', adminRoutes)
@@ -22,10 +34,27 @@ app.use(shopRoutes)
 // Any other routes not matching
 app.use(errorController.get404)
 
-sequelize.sync().then(result => {
-    // console.log(result)
-}).catch(err => {
-    console.log(err)
-})
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' })
+User.hasMany(Product)
+
+sequelize.
+    // ! force:true will overide database
+    sync()
+    .then(result => {
+        return User.findByPk(1)
+    })
+    .then(user => {
+        if (!user) {
+            return User.create({ name: 'Har', email: 'test@email.com' })
+        }
+        // * Returning a value in a then block automatically wrapped in a new promise
+        return user;
+    })
+    .then(user => {
+        // console.log(user)
+    })
+    .catch(err => {
+        console.log(err)
+    })
 
 app.listen(3000)
