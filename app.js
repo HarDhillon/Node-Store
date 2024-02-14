94dea19f75212b5e10b33408e4b5b3857b26d78a
HarDhillon <har7173@gmail.com>
2024-02-14 15:55:15 +0000

    updated cart model and created cart-item model to use the database. Updated app.js to create cart on app start

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const errorController = require('./controllers/error');
 const sequelize = require('./util/database')
 const Product = require('./models/product')
 const User = require('./models/user')
+const Cart = require('./models/cart')
+const CartItem = require('./models/cart-item')
 
 app.set('view engine', 'ejs');
 // bodyParser is now part of express so we can actually just do this
@@ -36,9 +38,15 @@ app.use(errorController.get404)
 
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' })
 User.hasMany(Product)
+User.hasOne(Cart)
+Cart.belongsTo(User)
+// * We need to tell sequelize how they are connected
+Cart.belongsToMany(Product, { through: CartItem })
+Product.belongsToMany(Cart, { through: CartItem })
 
 sequelize.
     // ! force:true will overide database
+    // sync({ force: true })
     sync()
     .then(result => {
         return User.findByPk(1)
@@ -51,10 +59,11 @@ sequelize.
         return user;
     })
     .then(user => {
-        // console.log(user)
+        return user.createCart();
+    })
+    .then(cart => {
+        app.listen(3000)
     })
     .catch(err => {
         console.log(err)
     })
-
-app.listen(3000)
