bcc23e6eb6dee30ba8d0a31b129481f8c40d56b4
HarDhillon <har7173@gmail.com>
2024-02-16 13:10:01 +0000

    Fix: Typo in defining orderItem model

--- models/order-item.js
+++ models/order-item.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize')
 const sequelize = require('../util/database')
 
-const OrderItem = sequelize.define('order', {
+const OrderItem = sequelize.define('orderItem', {
     id: {
         type: Sequelize.INTEGER,
         autoIncrement: true,
