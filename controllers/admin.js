53c5293771e5195c00d97273e8385bc94763277f
HarDhillon <har7173@gmail.com>
2024-02-20 11:04:47 +0000

    Update to allow editing products
    - Enabled route for editing
    
    - Editted controller to use our Product save function
    
    - Updated Model Save function to edit a product if and ID is passed to the function

--- controllers/admin.js
+++ controllers/admin.js
@@ -25,53 +25,45 @@ exports.postAddProduct = (req, res, next) => {
         })
 }
 
-// exports.getEditProduct = (req, res, next) => {
-//     // Fetch query param
-//     const editMode = req.query.edit;
-//     if (!editMode) {
-//         res.redirect('/')
-//     }
-//     const prodId = req.params.productId
+exports.getEditProduct = (req, res, next) => {
+    // Fetch query param
+    const editMode = req.query.edit;
+    if (!editMode) {
+        res.redirect('/')
+    }
+    const prodId = req.params.productId
 
-//     // Fetch only products related to the user
-//     req.user
-//         .getProducts({ where: { id: prodId } })
-//         .then(products => {
-//             const product = products[0]
-//             res.render('admin/edit-product', {
-//                 pageTitle: 'Edit product',
-//                 path: '/admin/edit-product',
-//                 editing: editMode,
-//                 product: product
-//             })
-//         })
-//         .catch(err => console.log(err))
+    // Fetch only products related to the user
+    Product.findById(prodId)
+        .then(product => {
+            res.render('admin/edit-product', {
+                pageTitle: 'Edit product',
+                path: '/admin/edit-product',
+                editing: editMode,
+                product: product
+            })
+        })
+        .catch(err => console.log(err))
 
-// }
+}
 
-// exports.postEditProduct = (req, res, next) => {
-//     const prodId = req.body.productId
-//     const updatedTitle = req.body.title
-//     const updatedPrice = req.body.price
-//     const updatedImageUrl = req.body.imageUrl
-//     const updatedDescription = req.body.description
+exports.postEditProduct = (req, res, next) => {
+    const prodId = req.body.productId
+    const updatedTitle = req.body.title
+    const updatedPrice = req.body.price
+    const updatedImageUrl = req.body.imageUrl
+    const updatedDescription = req.body.description
 
-//     Product.findByPk(prodId)
-//         .then(product => {
-//             product.title = updatedTitle;
-//             product.price = updatedPrice;
-//             product.imageUrl = updatedImageUrl
-//             product.description = updatedDescription
-//             // save will execute sequilize for us
-//             return product.save()
-//         })
-//         .then(result => {
-//             console.log("Product Updated")
-//             res.redirect('/admin/products')
-//         })
-//         .catch(err => console.log(err))
+    const product = new Product(updatedTitle, updatedPrice, updatedDescription, updatedImageUrl, prodId)
 
-// }
+    product.save()
+        .then(result => {
+            console.log("Product Updated")
+            res.redirect('/admin/products')
+        })
+        .catch(err => console.log(err))
+
+}
 
 // exports.postDeleteProduct = (req, res, next) => {
 //     const prodId = req.body.productId
--- models/product.js
+++ models/product.js
@@ -2,20 +2,29 @@ const mongodb = require('mongodb')
 const getDb = require("../util/database").getDb
 
 class Product {
-    constructor(title, price, description, imageUrl) {
+    constructor(title, price, description, imageUrl, id) {
         this.title = title
         this.price = price
         this.description = description
         this.imageUrl = imageUrl
+        this._id = new mongodb.ObjectId(id)
     }
 
     save() {
-
         const db = getDb()
-        return db.collection('products').insertOne(this)
-            .then(result => {
-                console.log(result)
-            })
+        let dbOp;
+
+        // If ID passed to Product model we will want to update an existing rather than save
+        if (this._id) {
+            // ? updateOne needs to be told where to find the item being updated AND what to update. In this case, everything.
+            dbOp = db.collection('products').updateOne({ _id: this._id }, { $set: this })
+        } else {
+            dbOp = db.collection('products').insertOne(this)
+        }
+
+        return dbOp.then(result => {
+            // console.log(result)
+        })
             .catch(err => console.log(err))
     }
 
@@ -24,7 +33,7 @@ class Product {
         // ? Find returns a cursor
         return db.collection('products').find().toArray()
             .then(products => {
-                console.log(products)
+                // console.log(products)
                 return products
             })
             .catch(err => console.log(err))
