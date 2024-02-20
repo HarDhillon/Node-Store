b57c2bdc2bf0d69ecce7791d3ae04cbba91a71ed
HarDhillon <har7173@gmail.com>
2024-02-20 12:09:41 +0000

    Create user model and update app.js
    - Create new user model with save and find functions
    
    - Updates Product model to take in a userId when created. Product now stores user ID associated with product
    
    - Update app.js to find a hard coded user on app start and store the user in all requests.

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const shopRoutes = require('./routes/shop');
 const errorController = require('./controllers/error');
 
 const mongoConnect = require('./util/database').mongoConnect
+const User = require('./models/user')
 
 
 app.set('view engine', 'ejs');
@@ -19,13 +20,12 @@ app.use(express.static(path.join(rootDir, 'public')))
 
 // Store our sequelize object in our request.
 app.use((req, res, next) => {
-    // User.findByPk(1)
-    //     .then(user => {
-    //         req.user = user
-    //         next()
-    //     })
-    //     .catch(err => console.log(err))
-    next()
+    User.findById("65d490a9897b1b4c8d6695b5")
+        .then(user => {
+            req.user = user
+            next()
+        })
+        .catch(err => console.log(err))
 })
 
 // putting a /argument here will only run if the url is /admin/*something*
