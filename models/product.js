e44bc54457ff121d6776acfff791fff6ee95b467
HarDhillon <har7173@gmail.com>
2024-02-23 11:00:16 +0000

    Update User and Product model to use mongoose structure to create their schema.

--- models/product.js
+++ models/product.js
@@ -1,65 +1,29 @@
-const mongodb = require('mongodb')
-const getDb = require("../util/database").getDb
+const mongoose = require('mongoose')
 
-class Product {
-    constructor(title, price, description, imageUrl, id, userId) {
-        this.title = title
-        this.price = price
-        this.description = description
-        this.imageUrl = imageUrl
-        this._id = id ? new mongodb.ObjectId(id) : null
-        this.userId = userId
+const Schema = mongoose.Schema
+
+const productSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    imageUrl: {
+        type: String,
+        required: true
+    },
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
     }
+})
 
-    save() {
-        const db = getDb()
-        let dbOp;
-
-        // If ID passed to Product model we will want to update an existing rather than save
-        if (this._id) {
-            // ? updateOne needs to be told where to find the item being updated AND what to update. In this case, everything.
-            dbOp = db.collection('products').updateOne({ _id: this._id }, { $set: this })
-        } else {
-            dbOp = db.collection('products').insertOne(this)
-        }
-
-        return dbOp.then(result => {
-            // console.log(result)
-        })
-            .catch(err => console.log(err))
-    }
-
-    static fetchAll() {
-        const db = getDb()
-        // ? Find returns a cursor
-        return db.collection('products').find().toArray()
-            .then(products => {
-                // console.log(products)
-                return products
-            })
-            .catch(err => console.log(err))
-    }
-
-    static findById(prodId) {
-        const db = getDb()
-        //? find will always return a cursor even if we find just one item so rather than use find().next() we can use findOne()
-        // * Id's are stored in a unique way in MongoDB so you need to convert your regular ID into an Object ID
-        return db.collection('products').findOne({ _id: new mongodb.ObjectId(prodId) })
-            .then(product => {
-                return product
-            })
-            .catch(err => console.log(err))
-    }
-
-    static deleteById(prodId) {
-        const db = getDb()
-
-        return db.collection('products').deleteOne({ _id: new mongodb.ObjectId(prodId) })
-            .then(result => {
-                console.log("Deleted Product")
-            })
-            .catch(err => console.log(err))
-    }
-}
-
-module.exports = Product
+module.exports = mongoose.model('Product', productSchema)
--- models/user.js
+++ models/user.js
@@ -1,137 +1,27 @@
-const mongodb = require('mongodb')
-const getDb = require("../util/database").getDb
+// const mongodb = require('mongodb')
+// const getDb = require("../util/database").getDb
 
-class User {
-    constructor(username, email, cart, id) {
-        this.name = username
-        this.email = email
-        this.cart = cart // {items: []}
-        this._id = id
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    cart: {
+        items: [
+            {
+                productId: { type: Schema.Types.ObjectId, required: true, ref: 'Product' },
+                quantity: { type: Number, required: true }
+            }
+        ]
     }
+})
 
-    save() {
-        const db = getDb()
-
-        return db.collection('users').insertOne(this)
-    }
-
-    addToCart(product) {
-
-        // if existing product id found in cart, will return its index (0 or more)
-        const cartProductIndex = this.cart.items.findIndex(cp => {
-            // * The retrieved id from the product collection is not actually a string
-            return cp.productId.toString() === product._id.toString()
-        })
-
-        let newQuantity = 1
-        const updatedCartItems = [...this.cart.items]
-
-        // If product exists, increase quantity by 1 and update in the array
-        if (cartProductIndex >= 0) {
-            newQuantity = this.cart.items[cartProductIndex].quantity + 1
-            updatedCartItems[cartProductIndex].quantity = newQuantity
-            // else push new item into array
-        } else {
-            updatedCartItems.push({
-                productId: new mongodb.ObjectId(product._id),
-                quantity: newQuantity
-            })
-        }
-
-        const updatedCart = {
-            items: updatedCartItems
-        }
-
-        const db = getDb()
-
-        return db.collection('users').updateOne(
-            { _id: new mongodb.ObjectId(this._id) },
-            { $set: { cart: updatedCart } })
-    }
-
-    getCart() {
-        const db = getDb()
-
-        // fetch all product id's in user collection
-        const productIds = this.cart.items.map(i => {
-            return i.productId
-        })
-        // find all elements in products where _id is in the array (returns cursor)
-        return db.collection('products')
-            .find({ _id: { $in: productIds } })
-            .toArray()
-            .then(products => {
-                return products.map(p => {
-                    // return the product details and add quantity by matching the id to user cart items where quantity is stored
-                    return {
-                        // ? find that product in user document
-                        ...p, quantity: this.cart.items.find(i => {
-                            return i.productId.toString() === p._id.toString()
-                        }).quantity
-                    }
-                })
-            })
-    }
-
-    deleteItemFromCart(prodId) {
-        const db = getDb()
-
-        // Return array without the deleted item
-        const updatedCartItems = this.cart.items.filter(item => {
-            return item.productId.toString() !== prodId.toString()
-        })
-
-        return db.collection('users').updateOne(
-            { _id: new mongodb.ObjectId(this._id) },
-            { $set: { cart: { items: updatedCartItems } } }
-        )
-
-    }
-
-    addOrder() {
-        const db = getDb();
-        // Reuse getCart to get enriched product information rather than just their id
-        return this.getCart()
-            .then(produts => {
-                const order = {
-                    items: produts,
-                    user: {
-                        _id: new mongodb.ObjectId(this._id),
-                        name: this.name,
-                        email: this.email
-                    }
-                }
-                return db.collection('orders').insertOne(order)
-            })
-            // Once successfully inserted order, update cart
-            .then(result => {
-                // update local User cart to empty
-                this.cart = { items: [] }
-
-                // Update cart in database to empty
-                return db.collection('users').updateOne(
-                    { _id: new mongodb.ObjectId(this._id) },
-                    { $set: { cart: { items: [] } } }
-                )
-            })
-            .catch(err => console.log())
-    }
-
-    getOrders() {
-        const db = getDb()
-
-        return db.collection('orders').find({ "user._id": new mongodb.ObjectId(this._id) }).toArray()
-    }
-
-    static findById(userId) {
-        const db = getDb()
-
-        return db.collection('users').findOne({ _id: new mongodb.ObjectId(userId) })
-            .then(user => {
-                return user
-            })
-            .catch(err => console.log(err))
-    }
-}
-
-module.exports = User
+module.exports = mongoose.model('User', userSchema)
