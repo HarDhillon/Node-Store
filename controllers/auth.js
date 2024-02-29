bb8c4585484493b6f30dc5d58251b3546cd67d50
HarDhillon <har7173@gmail.com>
2024-02-29 14:45:50 +0000

    Creates password reset functionality
    - Create two new views, to submit email of user needing reset and view to reset password
    
    - Create controller functions to allow user to reset their password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcryptjs')
 const nodemailer = require('nodemailer')
+const crypto = require('crypto')
 
 const User = require('../models/user');
 
@@ -126,3 +127,108 @@ exports.postLogout = (req, res, next) => {
     res.redirect('/');
   });
 };
+
+exports.getReset = (req, res, next) => {
+  let message = req.flash('error')
+  if (message.length > 0) {
+    message = message[0]
+  } else {
+    message = null
+  }
+
+  res.render('auth/reset', {
+    path: '/reset',
+    pageTitle: 'Reset Password',
+    errorMessage: message
+  })
+}
+
+exports.postReset = (req, res, next) => {
+  // Generate token to send to user email
+  let token
+  crypto.randomBytes(32, (err, buffer) => {
+    if (err) {
+      console.log(err)
+      return res.redirect('reset')
+    }
+    token = buffer.toString('hex')
+  })
+
+  // Check if user with email exists
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (!user) {
+        req.flash('error', 'No Email found')
+        return res.redirect('/reset')
+      }
+      user.resetToken = token
+      // Token to expire in 1 hour
+      user.resetTokenExpiration = Date.now() + 3600000
+      return user.save()
+    })
+    .then(result => {
+      res.redirect('/login')
+      return transport.sendMail({
+        to: req.body.email,
+        from: 'shop@demomailtrap.com',
+        subject: 'Password Reset',
+        html: `
+          <p>Click this <a href="http://localhost:3000/reset/${token}">link</a> to reset your password `
+      })
+    })
+    .catch(err => console.log(err))
+
+}
+
+exports.getNewPassword = (req, res, next) => {
+
+  const token = req.params.token
+  // $gt is a greater than comparison
+  User.findOne({ resetToken: token, resetTokenExpiration: { $gt: Date.now() } })
+    .then(user => {
+
+      let message = req.flash('error')
+      if (message.length > 0) {
+        message = message[0]
+      } else {
+        message = null
+      }
+
+      res.render('auth/new-password', {
+        path: '/new-password',
+        pageTitle: 'New Password',
+        errorMessage: message,
+        userId: user._id.toString(),
+        passwordToken: token
+      })
+    })
+    .catch(err => console.log(err))
+
+}
+
+exports.postNewPassword = (req, res, next) => {
+  const { newPassword, userId, passwordToken } = req.body
+  let resetUser
+
+  User.findOne({
+    resetToken: passwordToken,
+    resetTokenExpiration: { $gt: Date.now() },
+    _id: userId
+  })
+    .then(user => {
+      resetUser = user
+      return bcrypt.hash(newPassword, 12)
+    })
+    .then(hashedPassword => {
+      resetUser.password = hashedPassword;
+      // No longer need the token values
+      resetUser.resetToken = undefined
+      resetUser.resetUser = undefined
+      return resetUser.save()
+    })
+    .then(result => {
+      res.redirect('/login')
+    })
+    .catch(err => console.log(err))
+
+}
